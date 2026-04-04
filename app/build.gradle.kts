0ab4d6f53a21d68b96e71566aaf199a9567362cc
dhchang <huyentrang111120044@gmail.com>
2026-04-04 10:38:13 +0700

    Initialize Android project structure with essential files and configurations

---
+++ app/build.gradle.kts
@@ -0,0 +1,76 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.androidx.navigation.safeargs.kotlin)
+}
+
+android {
+    namespace = "com.n3t.mobile"
+    compileSdk {
+        version = release(36) {
+            minorApiLevel = 1
+        }
+    }
+
+    defaultConfig {
+        applicationId = "com.n3t.mobile"
+        minSdk = 24
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    buildFeatures {
+        viewBinding = true
+        buildConfig = true
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.core.splashscreen)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.androidx.preference.ktx)
+    implementation(platform(libs.koin.bom))
+    implementation(libs.koin.android)
+    implementation(libs.koin.androidx.navigation)
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.converter.gson)
+    implementation(libs.gson)
+    implementation(libs.glide)
+    implementation(libs.room.runtime)
+    implementation(libs.room.ktx)
+    implementation(libs.kotlinx.coroutines.android)
+    implementation(libs.kotlinx.coroutines.play.services)
+    implementation(libs.google.play.services.location)
+    val mapBoxVersion = "11.3.0"
+    implementation("com.mapbox.maps:android:$mapBoxVersion") {
+        exclude(group = "com.mapbox.mapboxsdk", module = "mapbox-sdk-directions-models")
+        exclude(group = "com.mapbox.mapboxsdk", module = "mapbox-sdk-turf")
+        exclude(group = "com.mapbox.mapboxsdk", module = "mapbox-sdk-services")
+        exclude(group = "com.mapbox.mapboxsdk", module = "mapbox-sdk-directions-refresh-models")
+        exclude(group = "com.mapbox.mapboxsdk", module = "mapbox-sdk-core")
+    }
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
