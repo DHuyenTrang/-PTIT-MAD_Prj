3c1195e1b1a3ab6155c033025b56a98b49d89256
dhchang <huyentrang111120044@gmail.com>
2026-04-11 00:25:36 +0700

    feat: setup app entry point and resources
    - N3TApplication: Koin, Mapbox, Environment initialization
    - MainActivity: NavHostFragment host with splash screen
    - activity_main.xml: FragmentContainerView with nav_graph
    - build.gradle.kts: fix compileSdk syntax
    - strings.xml: Vietnamese string resources for all screens

--- app/src/main/java/com/n3t/mobile/N3TApplication.kt
+++ app/src/main/java/com/n3t/mobile/N3TApplication.kt
@@ -1,6 +1,27 @@
 package com.n3t.mobile
 
 import android.app.Application
+import com.n3t.mobile.data.api.Environment
+import com.n3t.mobile.di.appModule
+import com.mapbox.common.MapboxOptions
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
 
-class N3TApplication: Application() {
+class N3TApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        // Load environment
+        val flavor = Environment.loadFlavor(applicationContext)
+        Environment.setFlavor(flavor)
+
+        // Init Mapbox
+        MapboxOptions.accessToken = getString(R.string.mapbox_access_token)
+
+        // Init Koin DI
+        startKoin {
+            androidContext(this@N3TApplication)
+            modules(appModule)
+        }
+    }
 }
